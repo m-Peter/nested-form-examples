01838fdef4da78ee4e872ef09726e0685032fd3b
Peter Markou <markoupetr@gmail.com>
2014-06-29 13:43:00 +0300

    SignupForm is correctly rendered for the new template.

--- signup-example/after/app/forms/signup_form.rb
+++ signup-example/after/app/forms/signup_form.rb
@@ -8,4 +8,12 @@ class SignupForm < AbstractForm
   validates_presence_of :username
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
   validates_length_of :password, minimum: 6
+
+  def subscribed
+    model.subscribed_at
+  end
+
+  def subscribed=(checkbox)
+    model.subscribed_at = Time.zone.now if checkbox == "1"
+  end
 end
--- signup-example/after/app/models/user.rb
+++ signup-example/after/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  has_one :profile
 end
