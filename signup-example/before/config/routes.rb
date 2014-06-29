6ed6e4b10669d6306867c1349765cb2289ecbbb2
Peter Markou <markoupetr@gmail.com>
2014-06-29 12:03:27 +0300

    Generate the SessionsController.

--- signup-example/before/config/routes.rb
+++ signup-example/before/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users
+  resources :sessions
 end
