555c102b300ccdb914e324dc3511661f8cc283cd
Peter Markou <markoupetr@gmail.com>
2014-07-11 11:33:29 +0300

    Refactor the link_to_add/remove_fields helpers to use the HTML data-* attribute.

--- project-example/after/app/assets/javascripts/application.js
+++ project-example/after/app/assets/javascripts/application.js
@@ -15,18 +15,30 @@
 //= require turbolinks
 //= require_tree .
 
-function remove_fields(link, dynamic) {
-  if (dynamic) {
-    $(link).closest(".nested-fields").remove();
-  } else {
-    $(link).prev("input[type=hidden]").val("1");
-    $(link).closest(".nested-fields").hide();
-  }
-}
+(function($) {
+  $(document).on('click', '.remove_fields.dynamic, .remove_fields.existing', function(e) {
+    var $this = $(this), 
+        node_to_delete = $this.closest('.nested-fields');
 
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regex = new RegExp("new_" + association, "g");
-  
-  $(link).before(content.replace(regex, new_id));
-}
+    e.preventDefault();
+
+    if ($this.hasClass('dynamic')) {
+        node_to_delete.remove();
+    } else {
+        $this.prev("input[type=hidden]").val("1");
+        node_to_delete.hide();
+    }
+  });
+
+  $(document).on('click', '.add_fields', function(e) {
+    e.preventDefault();
+    
+    var $this = $(this),
+      association = $this.data('association'),
+      content = $this.data('association-insertion-template');
+
+    var new_id = new Date().getTime();
+    var regex = new RegExp("new_" + association, "g");
+    $this.before(content.replace(regex, new_id));
+  });
+})(jQuery);
