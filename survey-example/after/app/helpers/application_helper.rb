94d9ae5eee5c4c6adbb0bd941df53c78ce1217d8
Peter Markou <markoupetr@gmail.com>
2014-07-11 11:36:02 +0300

    Refactor the link_to_add/remove_fields helpers to use the HTML data-* attribute.

--- survey-example/after/app/helpers/application_helper.rb
+++ survey-example/after/app/helpers/application_helper.rb
@@ -1,19 +1,33 @@
 module ApplicationHelper
   def link_to_remove_fields(name, f, options = {})
-    if f.object.persisted?
-      f.hidden_field(:_destroy) + link_to(name, '#', onclick: "remove_fields(this, false); return false;")
+    html_options = {}
+    is_existing = f.object.persisted?
+    
+    classes = []
+    classes << "remove_fields"
+    classes << (is_existing ? 'existing' : 'dynamic')
+    html_options[:class] = [classes.join(' ')]
+
+    if is_existing
+      f.hidden_field(:_destroy) + link_to(name, '#', html_options)
     else
-      link_to(name, '#', onclick: "remove_fields(this, true); return false;")
+      link_to(name, '#', html_options)
     end
   end
 
   def link_to_add_fields(name, f, association, options = {})
     new_object = f.object.get_model(association)
+    assoc = association.to_s.singularize
     
-    fields = f.fields_for(association, new_object, :child_index => "new_#{ association }") do |builder|
+    fields = f.fields_for(association, new_object, :child_index => "new_#{assoc}") do |builder|
       render(association.to_s, :f => builder)
     end
 
-    link_to name, '#', onclick: "add_fields(this, \"#{ association }\", \"#{ escape_javascript(fields) }\"); return false;"
+    html_options = {}
+    html_options[:class] = "add_fields"
+    html_options[:'data-association'] = assoc
+    html_options[:'data-association-insertion-template'] = CGI.escapeHTML(fields).html_safe
+
+    link_to(name, '#', html_options)
   end
 end
