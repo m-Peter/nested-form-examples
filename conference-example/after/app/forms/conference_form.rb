8acc09f146311695b92f907634d31e4498c3511f
Peter Markou <markoupetr@gmail.com>
2014-07-17 14:54:03 +0300

    Update the ConferenceForm class to use inline validation rules for presence.

--- conference-example/after/app/forms/conference_form.rb
+++ conference-example/after/app/forms/conference_form.rb
@@ -1,17 +1,11 @@
 class ConferenceForm < AbstractForm
-  attributes :name, :city
+  attributes :name, :city, required: true
 
   association :speaker do
-    attribute :name, :occupation
+    attribute :name, :occupation, required: true
 
     association :presentations, records: 2 do
-      attribute :topic, :duration
-
-      validates :topic, :duration, presence: true
+      attribute :topic, :duration, required: true
     end
-
-    validates :name, :occupation, presence: true
   end
-
-  validates :name, :city, presence: true
 end
