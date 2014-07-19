9bcaaa3d2e42918f2d8359d56ab30b6ee5f4515f
Peter Markou <markoupetr@gmail.com>
2014-07-19 11:07:04 +0300

    Add the allow_destroy option to accepts_nested_attributes_for and update the white list of params.

--- full-example/before/app/models/project.rb
+++ full-example/before/app/models/project.rb
@@ -1,14 +1,14 @@
 class Project < ActiveRecord::Base
-  has_many :tasks
-  has_many :people
-  belongs_to :owner, :class_name => 'Person'
+  has_many :tasks, dependent: :destroy
+  has_many :people, dependent: :destroy
+  belongs_to :owner, :class_name => 'Person', dependent: :destroy
 
-  has_many :project_tags
-  has_many :tags, :through => :project_tags, :class_name => 'Tag'
+  has_many :project_tags, dependent: :destroy
+  has_many :tags, :through => :project_tags, :class_name => 'Tag', dependent: :destroy
 
   accepts_nested_attributes_for :tasks, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :people, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :owner, :reject_if => :all_blank
-  accepts_nested_attributes_for :tags
-  accepts_nested_attributes_for :project_tags
+  accepts_nested_attributes_for :tags, :allow_destroy => true
+  accepts_nested_attributes_for :project_tags, :allow_destroy => true
 end
