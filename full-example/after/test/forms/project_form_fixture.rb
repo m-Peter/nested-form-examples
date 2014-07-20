25f50c9d0aa1eb074ffe332cef2632a207c6eaef
Peter Markou <markoupetr@gmail.com>
2014-07-20 11:23:14 +0300

    Create and update a Project with its Tasks.

--- full-example/after/test/forms/project_form_fixture.rb
+++ full-example/after/test/forms/project_form_fixture.rb
@@ -1,7 +1,8 @@
 class ProjectFormFixture < AbstractForm
   attributes :name, :description, required: true
 
-  association :tasks do
-    attributes :name, :description, :done, required: true
+  association :tasks, records: 2 do
+    attributes :name, :description, required: true
+    attribute :done
   end
 end
--- full-example/after/test/forms/project_form_test.rb
+++ full-example/after/test/forms/project_form_test.rb
@@ -37,7 +37,7 @@ class ProjectFormTest < ActiveSupport::TestCase
 
   test "tasks sub-form contains association name and parent" do
     assert_equal :tasks, @tasks_form.association_name
-    assert_equal 1, @tasks_form.records
+    assert_equal 2, @tasks_form.records
     assert_equal @project, @tasks_form.parent
   end
 
@@ -59,7 +59,7 @@ class ProjectFormTest < ActiveSupport::TestCase
 
   test "project form initializes the number of records specified" do
     assert_respond_to @tasks_form, :models
-    assert_equal 1, @tasks_form.models.size
+    assert_equal 2, @tasks_form.models.size
     
     @tasks_form.each do |form|
       assert_instance_of Form, form
@@ -74,7 +74,7 @@ class ProjectFormTest < ActiveSupport::TestCase
       assert_respond_to form, :done=
     end
 
-    assert_equal 1, @form.model.tasks.size
+    assert_equal 2, @form.model.tasks.size
   end
 
   test "project form fetches models for existing parent" do
@@ -83,7 +83,97 @@ class ProjectFormTest < ActiveSupport::TestCase
     form = ProjectFormFixture.new(project)
 
     assert_equal project.name, form.name
-    assert_equal 1, form.tasks.size
+    assert_equal 2, form.tasks.size
     assert_equal project.tasks[0], form.tasks[0].model
+    assert_equal project.tasks[1], form.tasks[1].model
+  end
+
+  test "project form syncs its model and the models in nested sub-forms" do
+    params = {
+      name: "Add Form Models",
+
+      tasks_attributes: {
+        "0" => { name: "Form unit", description: "Form to represent a single model", done: false },
+        "1" => { name: "Form collection", description: "A collection of Forms", done: false },
+      }
+    }
+
+    @form.submit(params)
+
+    assert_equal "Add Form Models", @form.name
+
+    assert_equal "Form unit", @form.tasks[0].name
+    assert_equal "Form to represent a single model", @form.tasks[0].description
+    assert_equal false, @form.tasks[0].done
+
+    assert_equal "Form collection", @form.tasks[1].name
+    assert_equal "A collection of Forms", @form.tasks[1].description
+    assert_equal false, @form.tasks[1].done
+
+    assert_equal 2, @form.tasks.size
+  end
+
+  test "project form saves its model and the models in nested sub-forms" do
+    params = {
+      name: "Add Form Models",
+      description: "Nested models in a single form",
+
+      tasks_attributes: {
+        "0" => { name: "Form unit", description: "Form to represent a single model", done: "0" },
+        "1" => { name: "Form collection", description: "A collection of Forms", done: "0" },
+      }
+    }
+
+    @form.submit(params)
+
+    assert_difference('Project.count') do
+      @form.save
+    end
+
+    assert_equal "Add Form Models", @form.name
+    assert_equal "Nested models in a single form", @form.description
+
+    assert_equal "Form unit", @form.tasks[0].name
+    assert_equal "Form to represent a single model", @form.tasks[0].description
+    assert_equal false, @form.tasks[0].done
+
+    assert_equal "Form collection", @form.tasks[1].name
+    assert_equal "A collection of Forms", @form.tasks[1].description
+    assert_equal false, @form.tasks[1].done
+
+    assert_equal 2, @form.tasks.size
+
+    assert @form.persisted?
+    @form.tasks.each do |task_form|
+      assert task_form.persisted?
+    end
+  end
+
+  test "project form updates its model and the models in nested sub-forms" do
+    project = projects(:yard)
+    form = ProjectFormFixture.new(project)
+    params = {
+      name: "Life",
+      
+      tasks_attributes: {
+        "0" => { name: "Eat", done: "1", id: tasks(:rake).id },
+        "1" => { name: "Pray", done: "1", id: tasks(:paint).id },
+      }
+    }
+
+    form.submit(params)
+
+    assert_difference('Project.count', 0) do
+      form.save
+    end
+
+    assert_equal "Life", form.name
+    assert_equal "Eat", form.tasks[0].name
+    assert_equal true, form.tasks[0].done
+    
+    assert_equal "Pray", form.tasks[1].name
+    assert_equal true, form.tasks[1].done
+    
+    assert_equal 2, form.tasks.size
   end
 end
